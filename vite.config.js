e8df213a0bd56583b483d32b8ea327407bd28d09
Noah Cohn <noahc42@gmail.com>
2026-01-09 05:03:25 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  name: 'add-css-charset',
+  transformIndexHtml(html) {
+        // 1. Find the CSS link that Vite injected
+        // 2. Add charset="utf-8" to it
+  return html.replace(
+  /<link rel="stylesheet"/g, 
+  '<link rel="stylesheet" charset="utf-8"'
+  );
+  }
 })
